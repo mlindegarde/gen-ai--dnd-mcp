606c4a988abf48c2601fbf60a358811fed54343f
Michael Lindegarde <lindegar@amazon.com>
2025-10-15 14:36:23 -0700

    clean-up.

--- start_mcp_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting D&D Character Sheet Filler MCP Server..."
-echo "Server will listen on stdin/stdout for JSON-RPC messages"
-echo "Press Ctrl+C to stop"
-echo ""
-cd "$(dirname "$0")"
-cargo run
